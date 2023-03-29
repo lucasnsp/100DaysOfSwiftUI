216a46d09b5b86b7b37b512b5cf98fd56db93e27
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-29 11:50:55 -0300

    Add files via upload

---
+++ Day 02 - Booleans, string interpolation, and checkpoint 1/How to store truth with Booleans.playground/Checkpoint 1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+let temperatureInCelsius = 16
+
+let temperatureInFahrenheit = temperatureInCelsius * 9 / 2 + 32
+
+print(temperatureInCelsius)
+print(temperatureInFahrenheit)
+
+var message = "The temperature in Celsius is \(temperatureInCelsius) and in Fahrenheit is \(temperatureInFahrenheit) its a cold day. "
+
+print(message)
