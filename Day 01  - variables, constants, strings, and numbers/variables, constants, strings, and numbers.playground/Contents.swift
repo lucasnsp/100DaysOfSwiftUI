ff367848d5defd00a482bc5f2abea1e65632de7a
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-29 11:07:28 -0300

    Add files via upload

---
+++ Day 01  - variables, constants, strings, and numbers/variables, constants, strings, and numbers.playground/Contents.swift
@@ -0,0 +1,75 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name =  "Ted"
+
+name = "Rebecca"
+name = "Keeley"
+
+let chacaracter = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+
+// How to create Strings
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+
+let result = "⭐️ You Win! ⭐️"
+
+let movie = """
+A day in
+the life of an
+Apple enginer
+"""
+
+print(actor.count)
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+// How to Store while numbers
+
+let score = 10
+
+let reallyBig = 100_000_000
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+// How to store decimal Numbers
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 5.0
+rating *= 2
+
+
+
