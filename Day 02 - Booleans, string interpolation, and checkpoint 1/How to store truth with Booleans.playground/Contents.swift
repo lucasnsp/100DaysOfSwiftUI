f7b55ba926aaa95ac491b0df72a0e87848ebd711
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-29 11:28:35 -0300

    Add files via upload

---
+++ Day 02 - Booleans, string interpolation, and checkpoint 1/How to store truth with Booleans.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+
+let goodDogs = true
+var gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+
