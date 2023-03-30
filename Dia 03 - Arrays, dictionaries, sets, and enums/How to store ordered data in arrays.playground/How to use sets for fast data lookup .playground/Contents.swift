19cb72ae4cd83e5d0939f05a5e76e2dac0fa983e
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-30 10:50:00 -0300

    Add files via upload

---
+++ Dia 03 - Arrays, dictionaries, sets, and enums/How to store ordered data in arrays.playground/How to use sets for fast data lookup .playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+// How to use sets for fast data lookup
+
+var actors = Set<String>()
+actors.insert("Denzel Washington")
+actors.insert("Tom Cruise")
+actors.insert("Nicolas Cage")
+actors.insert("Samuel L Jacskson")
+print(actors)
