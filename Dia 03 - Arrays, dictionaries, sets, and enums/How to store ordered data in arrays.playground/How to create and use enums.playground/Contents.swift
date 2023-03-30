0992bceeaa477daf8891439ba25de2dc3e65211f
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-03-30 11:13:54 -0300

    Add files via upload

---
+++ Dia 03 - Arrays, dictionaries, sets, and enums/How to store ordered data in arrays.playground/How to create and use enums.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+// How to create and use enums
+
+
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
