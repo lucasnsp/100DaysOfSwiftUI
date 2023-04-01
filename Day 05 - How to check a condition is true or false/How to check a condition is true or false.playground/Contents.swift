f7b6d859eacb3f6ce209ee0c78e10da537f3ea54
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-01 10:01:44 -0300

    Add files via upload

---
+++ Day 05 - How to check a condition is true or false/How to check a condition is true or false.playground/Contents.swift
@@ -0,0 +1,72 @@
+import Cocoa
+
+// How to check a condition is true or false
+
+//if someCondition {
+//    print("Do somenthing")
+//}
+
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going, we don't need roads")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test")
+}
+
+if age >= 18 {
+    print("You 're eligible to vote")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimmer"
+
+if ourName > friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+
+var numbers = [1, 2, 3]
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.remove(at: 0)
+}
+
+print(numbers)
+
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G'day!")
+}
+
+let name = "Taylor Swift"
+
+if name != "Anonymus" {
+    print("Welcome, \(name)")
+}
+
+
+var username = "taylorswift13"
+
+if username.isEmpty == true{
+    username = "Anonymus"
+}
+
+print("Welcome \(username)")
