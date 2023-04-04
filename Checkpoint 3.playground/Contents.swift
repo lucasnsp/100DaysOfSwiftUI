ee520fbee4572cfec62f0215028a1c60f8b3dcfa
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-04 10:00:49 -0300

    Add files via upload

---
+++ Checkpoint 3.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+// checkpoint 3
+
+/// Your goal is to loop foram 1 trough 100, and for each number:
+/// If its a multiple of 3, print "fizz)
+/// if its a multiple of 5 , print buzz
+/// if its a multiple of 3 and 5 print "fizzbuzz"
+/// otherwise, just print the number
+
+var multipleOf3 = 3
+var multipleOf5 = 5
+
+for i in 1...100 {
+    if i % 3 == 0 && i % 5 == 0 {
+        print("FizzBuzz")
+    } else if i % 3 == 0 {
+        print("Fizz")
+    } else if i % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
