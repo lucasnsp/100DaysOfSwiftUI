bd2ba4568307be12e4e54418fb775102b30c13ed
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-06 10:32:28 -0300

    Add files via upload

---
+++ Day 08 - default values, throwing functions, and checkpoint 4/default values, throwing functions, and checkpoint 4.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+// default values, throwing functions, and checkpoint 4
+
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+ 
