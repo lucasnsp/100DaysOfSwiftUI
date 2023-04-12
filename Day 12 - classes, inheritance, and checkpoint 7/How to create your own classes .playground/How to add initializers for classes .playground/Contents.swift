6b5f853a0a758f1020cf62e29d1691b5f28719d1
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-12 11:40:46 -0300

    Add files via upload

---
+++ Day 12 - classes, inheritance, and checkpoint 7/How to create your own classes .playground/How to add initializers for classes .playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+// How to add initializers for classes
+
+class Vehicle {
+    let isElectric: Bool
+    
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+    
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+
+let teslaX = Car(isElectric: true, isConvertible: false)
