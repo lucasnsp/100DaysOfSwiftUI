d5c2404f918a310c38aab8fbdaabe503fc64cd66
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-13 08:54:51 -0300

    Add files via upload

---
+++ Day 12 - classes, inheritance, and checkpoint 7/How to create your own classes .playground/How to copy classes .playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+// How to copy classes
+
+class User {
+    var username = "Anonymous"
+    
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
+}
+
+var user1 = User()
+var user2 = user1.copy()
+user2.username = "Taylor"
+
+print(user1.username)
+print(user2.username)
