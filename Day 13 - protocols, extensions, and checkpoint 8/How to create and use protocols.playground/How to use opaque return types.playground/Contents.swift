9105b4e86c3df538a8537078a7544b3c2d24ed6c
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-14 11:47:25 -0300

    Add files via upload

---
+++ Day 13 - protocols, extensions, and checkpoint 8/How to create and use protocols.playground/How to use opaque return types.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+// How to use opaque return types
+
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+func getRandomBool() -> some Equatable {
+    Bool.random()
+}
+
+print(getRandomNumber() == getRandomNumber())
