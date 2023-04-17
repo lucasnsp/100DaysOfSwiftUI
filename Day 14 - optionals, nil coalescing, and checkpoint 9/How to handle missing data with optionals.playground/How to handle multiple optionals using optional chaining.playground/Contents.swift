cbec590355699d1d375fe106757b8cc350d4db54
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-17 15:20:12 -0300

    Add files via upload

---
+++ Day 14 - optionals, nil coalescing, and checkpoint 9/How to handle missing data with optionals.playground/How to handle multiple optionals using optional chaining.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+// How to handle multiple optionals using optional chaining
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
